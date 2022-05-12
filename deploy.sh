7bd1fd677cf993752ac263ce7846725ac0c4bcce
Elijah Abgao <elijah.abgao@fligno.com>
2022-05-13 00:40:07 +0800

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:skeltonmod/TheWashSvelte.git master:gh-pages
+git push -f git@github.com:skeltonmod/TheWashSvelte.git main:gh-pages
 
 cd -
