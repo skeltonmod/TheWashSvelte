60c18da8890f2cd8cae41a81d869c32b25b81789
novidash <abgao@gmail.com>
2023-03-09 20:50:05 +0800

    Added warning issue

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -33,6 +33,9 @@ export default function Search() {
         <center>
           <h4>Fucking Media Sideloader Bullshit</h4>
         </center>
+        <center>
+          <h4 style="color:red;">Anime provider is currently down (service is unreachable) check back later or watch other media!</h4>
+        </center>
         <div class="form-group">
           <input
             type="text"
